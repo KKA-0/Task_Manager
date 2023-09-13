dd5f4488755d5235768157e83290f1db4181ee9f
KKA-0 <karanyobro@gmail.com>
2023-09-14 00:00:23 +0530

    kuch to hai

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const morgan = require('morgan')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./Routers/crudRoute')
 const port = 6969
 
 app.use(bodyParser.json())
-
+app.use(cors())
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
 const DB = "mongodb+srv://admin:admin123@cluster0.un0qo4j.mongodb.net/";
 mongoose.connect(DB, {})
     .then(() => console.log("Connected to the DataBase 🧬"))
